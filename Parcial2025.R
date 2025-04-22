5fdc9269428308eedcb5180349ef6323cbe80479
ManuelRomero2025 <manuelromero20241@gmail.com>
2025-04-22 11:37:29 -0500

    Create Parcial2025.R

---
+++ Parcial2025.R
@@ -0,0 +1,28 @@
+#PARCIAL
+library(ggplot2)
+set.seed(9087)
+Base.A=round(rnorm(100,30,10),0)
+#####
+clase=1+3.33*log10(length(Base.A))
+clase=round(clase,0)
+ancho=diff(range(Base.A))/clase
+ancho=round(ancho,0)
+####
+bin=seq(min(Base.A),
+        max(Base.A),by=ancho)
+
+T1=data.frame(table(cut(Base.A,
+                     breaks =c(bin,Inf),
+                     right = FALSE)))
+
+ggplot(T1,aes(x=Var1,
+                    y=Freq))+
+  geom_bar(stat = "identity")+
+  labs(title="math_score",
+       x="Marca de clase")+
+  geom_text(aes(label=Freq),
+            position = position_dodge(0.5),
+            color="black",
+            vjust=1,
+            size=3)
+
