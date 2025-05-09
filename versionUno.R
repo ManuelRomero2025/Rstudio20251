e23b245c1a565cd26491c6f65385f38807a26b99
ManuelRomero2025 <manuelromero20241@gmail.com>
2025-05-09 10:04:23 -0500

    Update versionUno.R
    Actualización para hacer cortes con cut

--- versionUno.R
+++ versionUno.R
@@ -2,3 +2,15 @@ a=c(1,2,3,4,5,5)
 mean(a)
 median(a)
 sd(a)
+
+############3
+
+data=round(rnorm(120, 120, 15),0)
+quantile(data)
+cut=cut(data, breaks=c(quantile(data), Inf), 
+        right = FALSE)
+
+df=data.frame(data=data, Intervalos=cut)
+head(df,100)
+summary(df)
+
